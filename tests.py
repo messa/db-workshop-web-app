11a281dc76197038e8beed081d015ae4619d4a5a
Petr Messner <petr.messner@gmail.com>
2017-03-15 01:21:41 +0100

    rozdeleni prototypu do funkci

--- tests.py
+++ tests.py
@@ -36,8 +36,7 @@ def test_smoke_sqlite3(temp_dir):
     conn.close()
 
 
-def test_workflow_prototype(temp_dir):
-    conn = sqlite3.connect(str(temp_dir / 'workflow.db'))
+def prepare_schema(conn):
     c = conn.cursor()
     c.execute('''
         CREATE TABLE suggestions (
@@ -56,26 +55,51 @@ def test_workflow_prototype(temp_dir):
             value INTEGER
         )
     ''')
-    c.execute(
-        "INSERT INTO suggestions (title, date, cookie) VALUES (?, ?, ?)",
-        ('Tvorba webu', datetime(2017, 3, 15, 12, 0).isoformat(), 'cookie1'))
-    sug1_id = c.lastrowid
-    c.execute(
-        "INSERT INTO suggestions (title, date, cookie) VALUES (?, ?, ?)",
-        ('Statistika', datetime(2017, 3, 15, 12, 10).isoformat(), 'cookie1'))
-    # add some upvotes
-    c.execute(
-        "INSERT INTO votes (suggestion_id, date, cookie, value) VALUES (?, ?, ?, ?)",
-        (sug1_id, datetime(2017, 3, 15, 13, 0).isoformat(), 'cookie1', 1))
-    c.execute(
-        "INSERT INTO votes (suggestion_id, date, cookie, value) VALUES (?, ?, ?, ?)",
-        (sug1_id, datetime(2017, 3, 15, 13, 0).isoformat(), 'cookie2', 1))
-    # add some downvote
-    c.execute(
-        "INSERT INTO votes (suggestion_id, date, cookie, value) VALUES (?, ?, ?, ?)",
-        (sug1_id, datetime(2017, 3, 15, 13, 0).isoformat(), 'cookie3', -1))
 
+
+def test_workflow_prototype(temp_dir):
+    conn = sqlite3.connect(str(temp_dir / 'workflow.db'))
+    prepare_schema(conn)
+    sug1_id = insert_suggestion(
+        conn, 'Tvorba webu', 'cookie1', date=datetime(2017, 3, 15, 12, 0))
+    sug2_id = insert_suggestion(
+        conn, 'Statistika', 'cookie1', date=datetime(2017, 3, 15, 13, 0))
+    # add some upvotes
+    insert_upvote(conn, sug1_id, 'cookie1', upvote=True, date=datetime(2017, 3, 15, 13, 0))
+    insert_upvote(conn, sug1_id, 'cookie2', upvote=True, date=datetime(2017, 3, 15, 13, 0))
+    insert_upvote(conn, sug1_id, 'cookie3', upvote=False, date=datetime(2017, 3, 15, 13, 0))
     # the most important query :)
+    rows = list_suggestions(conn)
+    assert rows == [
+        {'title': 'Tvorba webu', 'vote_count': 1},
+        {'title': 'Statistika',  'vote_count': 0},
+    ]
+
+
+def insert_suggestion(conn, title, cookie, date=None):
+    date = date or datetime.utcnow()
+    c = conn.cursor()
+    c.execute(
+        "INSERT INTO suggestions (title, date, cookie) VALUES (?, ?, ?)",
+        (title, date.isoformat(), cookie))
+    return c.lastrowid
+
+
+def insert_upvote(conn, suggestion_id, cookie, upvote, date=None):
+    date = date or datetime.utcnow()
+    c = conn.cursor()
+    c.execute(
+        "INSERT INTO votes (suggestion_id, date, cookie, value) "
+        "VALUES (?, ?, ?, ?)", (
+            int(suggestion_id),
+            date.isoformat(),
+            cookie,
+            1 if upvote else -1,
+        ))
+
+
+def list_suggestions(conn):
+    c = conn.cursor()
     c.execute('''
         SELECT
             suggestions.title,
@@ -85,8 +109,10 @@ def test_workflow_prototype(temp_dir):
         GROUP BY suggestions.id
         ORDER BY vote_count DESC
     ''')
-    rows = c.fetchall()
-    assert rows == [
-        ('Tvorba webu', 1),
-        ('Statistika', 0),
-    ]
+    suggestions = []
+    for row in c:
+        suggestions.append({
+            'title': row[0],
+            'vote_count': row[1],
+        })
+    return suggestions
